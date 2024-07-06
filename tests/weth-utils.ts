cf35fb4470c865679228dbda08547239ea40a9fe
Alex Pedersen <hello@alexpedersen.dev>
2024-07-06 22:13:36 +0200

    Initialize subgraph

---
+++ tests/weth-utils.ts
@@ -0,0 +1,140 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  AdminRemoved,
+  Approval,
+  MinterAdded,
+  MinterRemoved,
+  SpenderUnwhitelisted,
+  SpenderWhitelisted,
+  Transfer
+} from "../generated/WETH/WETH"
+
+export function createAdminChangedEvent(
+  _oldAdmin: Address,
+  _newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("_oldAdmin", ethereum.Value.fromAddress(_oldAdmin))
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("_newAdmin", ethereum.Value.fromAddress(_newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createAdminRemovedEvent(_oldAdmin: Address): AdminRemoved {
+  let adminRemovedEvent = changetype<AdminRemoved>(newMockEvent())
+
+  adminRemovedEvent.parameters = new Array()
+
+  adminRemovedEvent.parameters.push(
+    new ethereum.EventParam("_oldAdmin", ethereum.Value.fromAddress(_oldAdmin))
+  )
+
+  return adminRemovedEvent
+}
+
+export function createApprovalEvent(
+  _owner: Address,
+  _spender: Address,
+  _value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("_owner", ethereum.Value.fromAddress(_owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("_spender", ethereum.Value.fromAddress(_spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("_value", ethereum.Value.fromUnsignedBigInt(_value))
+  )
+
+  return approvalEvent
+}
+
+export function createMinterAddedEvent(_minter: Address): MinterAdded {
+  let minterAddedEvent = changetype<MinterAdded>(newMockEvent())
+
+  minterAddedEvent.parameters = new Array()
+
+  minterAddedEvent.parameters.push(
+    new ethereum.EventParam("_minter", ethereum.Value.fromAddress(_minter))
+  )
+
+  return minterAddedEvent
+}
+
+export function createMinterRemovedEvent(_minter: Address): MinterRemoved {
+  let minterRemovedEvent = changetype<MinterRemoved>(newMockEvent())
+
+  minterRemovedEvent.parameters = new Array()
+
+  minterRemovedEvent.parameters.push(
+    new ethereum.EventParam("_minter", ethereum.Value.fromAddress(_minter))
+  )
+
+  return minterRemovedEvent
+}
+
+export function createSpenderUnwhitelistedEvent(
+  _spender: Address
+): SpenderUnwhitelisted {
+  let spenderUnwhitelistedEvent = changetype<SpenderUnwhitelisted>(
+    newMockEvent()
+  )
+
+  spenderUnwhitelistedEvent.parameters = new Array()
+
+  spenderUnwhitelistedEvent.parameters.push(
+    new ethereum.EventParam("_spender", ethereum.Value.fromAddress(_spender))
+  )
+
+  return spenderUnwhitelistedEvent
+}
+
+export function createSpenderWhitelistedEvent(
+  _spender: Address
+): SpenderWhitelisted {
+  let spenderWhitelistedEvent = changetype<SpenderWhitelisted>(newMockEvent())
+
+  spenderWhitelistedEvent.parameters = new Array()
+
+  spenderWhitelistedEvent.parameters.push(
+    new ethereum.EventParam("_spender", ethereum.Value.fromAddress(_spender))
+  )
+
+  return spenderWhitelistedEvent
+}
+
+export function createTransferEvent(
+  _from: Address,
+  _to: Address,
+  _value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("_from", ethereum.Value.fromAddress(_from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("_to", ethereum.Value.fromAddress(_to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("_value", ethereum.Value.fromUnsignedBigInt(_value))
+  )
+
+  return transferEvent
+}
